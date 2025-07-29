20a092b93f20426197498974c437709974e23b8c
GastonMora42 <gaston-mora@hotmail.com>
2025-07-29 14:43:43 -0300

    mejoramos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.8.2")
 
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
